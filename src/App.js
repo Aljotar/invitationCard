b43bac51f3b6eafae7a71011a658ba5e537f84f0
Alvaro Jotar <85590864+Aljotar@users.noreply.github.com>
2023-01-28 17:28:20 -0300

    finalizo estilo en mobile

--- src/App.js
+++ src/App.js
@@ -28,7 +28,11 @@ function App() {
       </div>
       
       <CountdownTimer />
-      <h3 className="bautismo text-center">Luego de la ceremonia te invito a festejar</h3>
+      <br/>
+      <br/>
+      <h3 className="bautismo text-center">Luego de la ceremonia te invito a festejar a las 21:30hs</h3>
+      <h3 className="papis text-center">En Corrientes 3369</h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.81786935005!2d-65.24485489999999!3d-26.813927300000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225cfbdb631305%3A0x8893c9c8a211c179!2sPcia%20de%20Corrientes%203369%2C%20T4000EDO%20San%20Miguel%20de%20Tucum%C3%A1n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1674936321469!5m2!1ses!2sar" width="100%" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 }
